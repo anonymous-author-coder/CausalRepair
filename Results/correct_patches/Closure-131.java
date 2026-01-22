39ae80663d8fa5100355ac92fd7a288cf61aa79a
anonymous-author-coder <anonymousauthor@126.com>
2026-01-23 00:43:29 +0800

    Update Closure-131.java

--- Results/correct_patches/Closure-131.java
+++ Results/correct_patches/Closure-131.java
@@ -1,21 +1,19 @@
 public static boolean isJSIdentifier(String s) {
     int length = s.length();
 
-    if (length == 0) {
+    if (length == 0 ||
+        Character.isIdentifierIgnorable(s.charAt(0)) ||
+        !Character.isJavaIdentifierStart(s.charAt(0))) {
         return false;
     }
-
-    char first = s.charAt(0);
-    if (!(Character.isLetter(first) || first == '_' || first == '$')) {
-        return false;
-    }
-
+    
     for (int i = 1; i < length; i++) {
-        char c = s.charAt(i);
-        if (!(Character.isLetterOrDigit(c) || c == '_' || c == '$')) {
+        if (Character.isIdentifierIgnorable(s.charAt(i)) ||
+            !Character.isJavaIdentifierPart(s.charAt(i))) {
             return false;
         }
     }
 
     return true;
+
 }
