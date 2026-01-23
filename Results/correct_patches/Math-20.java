1d2fb53ffc84033abea0fc3639be9eff0da6f577
anonymous-author-coder <anonymousauthor@126.com>
2026-01-23 18:27:46 +0800

    Update Math-20.java

--- Results/correct_patches/Math-20.java
+++ Results/correct_patches/Math-20.java
@@ -1,11 +1,6 @@
 public double[] repairAndDecode(final double[] x) {
-    // Alternative repair implementation using Math.min/max
     if (boundaries != null && isRepairMode) {
-        double[] repaired = new double[x.length];
-        for (int i = 0; i < x.length; i++) {
-            repaired[i] = Math.max(0, Math.min(1.0, x[i]));
-        }
-        return decode(repaired);
+        return decode(repair(x));
     }
     return decode(x);
 }
